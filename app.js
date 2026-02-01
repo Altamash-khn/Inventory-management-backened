a4b49c16245b187d2d10c3d67ed0ad581a17f110
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-02-01 13:09:21 +0530

    fix: switch cors import from ES module to CommonJS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import cors from "cors";
+const cors = require("cors");
 
 const express = require("express");
 const { connectToDatabase } = require("./data/database");
