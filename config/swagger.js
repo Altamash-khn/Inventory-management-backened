e7e5ad752d7cc8dc485af36f1643cdd1dabf6709
Altamash <Altamashkhn281@gmail.com>
2026-02-01 14:36:47 +0530

    Update Swagger server URL to production

--- config/swagger.js
+++ config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://inventory-management-backened.onrender.com",
       },
     ],
     components: {
